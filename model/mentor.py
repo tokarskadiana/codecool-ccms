79148d714f164cb23c1909d12b73bfc7b8045ffb
GitGitarra <monika.plocica@gmail.com>
2017-01-25 14:11:13 +0100

    mentor class

--- model/mentor.py
+++ model/mentor.py
@@ -1,26 +1,55 @@
 from user import User
-from mentor import *
 
 
 class Mentor(User):
     mentors_list = []
 
-    def add_mentor(self):
-        m = Mentor()
-        self.mentors_list.append(m)
 
+    @classmethod
+    def add_mentor(cls, password, first_name, last_name, telephone=None, mail=None):
+        m = Mentor(password, first_name, last_name, telephone, mail)
+        cls.mentors_list.append(m)
 
-    def edit_mentor(self, username):
+    def edit_mentor(self, **kwargs):
+        for key, value in kwargs.items():
+            if key:
+                if key in self.__dict__.keys():
+                    self.__dict__[key] = value
 
-        pass
+        return self.__dict__.items()
 
-    def view_mentor_details(self):
-        pass
+    def view_mentor_details(self, username):
+        for mentor in self.mentors_list:
+            if username == mentor.username:
+                return [self.username, self.first_name, self.last_name, self.telephone, self.mail]
 
-    def delete_mentor(self):
-        pass
+    @classmethod
+    def delete_mentor(cls, username):
+        for mentor in Mentor.mentors_list:
+            if username == mentor.username:
+                Mentor.mentors_list.remove(mentor)
+
+        return Mentor.mentors_list
 
     def list_mentors(self):
-        pass
+        return self.mentors_list
+
+
+
+#
+# Mentor.add_mentor('password', 'Marcin', 'Dupa', '123', '@@@')
+# Mentor.add_mentor('password', 'dupa', 'Dupa', '345', '@@@')
+# lista = Mentor.mentors_list
+#
+# print(lista)
+# print(Mentor.mentors_list[0].first_name)
+# Mentor.delete_mentor('dupa.Dupa')
+# print(lista)
+# Mentor.mentors_list[0].edit_mentor(first_name='Dupaaaaaaaaa')
+#
+# # Mentor.edit_mentor(Mentor.mentors_list[0], first_name='dupaaaaaaaaaaaaaaaaaaa')
+# print(lista)
+# print(Mentor.mentors_list[0].first_name)
+
 
 
