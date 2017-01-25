066b6b94055b441b4e57309ea29b4627440a665b
tokarskadiana <tokarskadiana@gmail.com>
2017-01-25 15:43:13 +0100

    Add controllers

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('../home/diana/Codecool/python-ccms-team10-1')
-from python-ccms-team10-1.model import User
 
 
 class UserController:
@@ -9,22 +6,5 @@ class UserController:
         Constructor of user controller.
 
         Arguments:user object
-        '''
-        pass
-
-    #  Maybe must be in separate class
-    def check_user_type(self):
-        '''
-        Check what type of user was loged in.
-
-        Returns:class type
-        '''
-        pass
-
-    def start_session(self):
-        '''
-        Create apropriate conntroller for user.
-
-        Returns: controller object
         '''
         pass
