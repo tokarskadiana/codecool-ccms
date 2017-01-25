90a43f9f561af6ed664f64ce331f3cf70153eb3e
poszlaka <sebastian.kt7@gmail.com>
2017-01-25 13:29:50 +0100

    add doc string

--- model/attendance.py
+++ model/attendance.py
@@ -1,43 +1,71 @@
 class Attendance:
-    list_of_attendance = [] # {date:{},date:{}}
+    list_of_attendance = [] # [obcj]  : obcj " ", {" ": 0/1}
 
-    def __init__(self, date, presence):
+    def __init__(self, date):
+        """
+        Initialize Assignment object.
+        :param date (str): date of checking attendance
+        """
         self.date = date
-        self.student_presence = presence
+        self.student_presence = {}
 
-    def list_attendance(self):
+    def check_attendance(self, username, present):
         """
+        Change value of presence.
+        :param username(str): username of student
+        :param present(int): change value of student presence (0 or 1)
+        """
+        if present in range(2):
+            self.student_presence[username] = present
 
-        :return:
+    @staticmethod
+    def list_attendance(username):
         """
-        pass
+        Return dict of overall presence of given username. day:value of present.
+        :return (dict): example {'10.10.2015': 0, '11.10.2015': 1}
+        """
+        present_list = {}
+        for days in Attendance.list_of_attendance:
+            for student, present in days.student_presence.items():
+                if username == student:
+                    present_list[days.date] = present
+        print(present_list)
+        return present_list
+
 
     def add(self):
         """
-
-        :return:
+        Add Attendance objc to list_of_attendance.
         """
-        self.date_presence = {self.date:self.student_presence}
-        Attendance.list_of_attendance.append(self.date_presence)
+        Attendance.list_of_attendance.append(self)
 
     @staticmethod
-    def present_statistic(list):
+    def present_statistic():
         """
-
-        :param data:
-        :return: SAMPLE DICT {'patrycja': '100', 'przemek': '50'}
+        Calculate overall present for each student.
+        :return (dict): SAMPLE DICT {'patrycja': '100', 'przemek': '50'}
         """
         percent_of_presence = {}
 
-        for attendance_list, students in list.items():
-                for student, attendance in students.items():
-                    if student in percent_of_presence.keys():
-                        print(student, attendance)
-                        percent_of_presence[student] += students[student]
-                    else:
-                        percent_of_presence[student] = students[student]
-
+        for day_attendance in Attendance.list_of_attendance:
+            for student, present in day_attendance.student_presence.items():
+                if student in percent_of_presence.keys():
+                    percent_of_presence[student] += present
+                else:
+                    percent_of_presence[student] = present
         for student, attendance in percent_of_presence.items():
-            percent_of_presence[student] = str(int((attendance/len(list))*100))
-
+            percent_of_presence[student] = str(int((attendance/len(Attendance.list_of_attendance))*100))
+        # print(percent_of_presence)
         return percent_of_presence
+
+# g = Attendance('10.10.2015')
+# g.check_attendance('przemek', 1)
+# g.check_attendance('patrycja', 0)
+# g.add()
+# w = Attendance('11.10.2015')
+# w.check_attendance('przemek', 1)
+# w.check_attendance('patrycja', 1)
+# w.add()
+# Attendance.present_statistic()
+# Attendance.list_attendance('patrycja')
+#
