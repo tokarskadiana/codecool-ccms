6604082f2112b09777a95b79931ff4176c943551
GitGitarra <monika.plocica@gmail.com>
2017-01-25 10:59:46 +0100

    add mentor add method

---
+++ model/mentor.py
@@ -0,0 +1,24 @@
+from user import User
+from mentor import *
+
+
+class Mentor(User):
+    mentors_list = []
+
+    def add_mentor(self):
+        m = Mentor()
+        self.mentors_list.append(m)
+
+
+    def edit_mentor(self, username):
+        if
+        pass
+
+    def view_mentor_details(self):
+        pass
+
+    def delete_mentor(self):
+        pass
+
+    def list_mentors(self):
+        pass
