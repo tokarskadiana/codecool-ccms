cef54e3725eb5afc5688473096f0db1d67d87652
GitGitarra <monika.plocica@gmail.com>
2017-01-25 14:13:57 +0100

    user

--- model/user.py
+++ model/user.py
@@ -1,8 +1,10 @@
-from student import *
-from mentor import Mentor
-from student import Student
+# from student import *
+# from mentor import Mentor
+# from student import Student
+
 
 class User:
+
     def __init__(self, password, first_name, last_name, telephone, mail):
         self.username = '{}.{}'.format(first_name, last_name)
 
@@ -13,18 +15,31 @@ class User:
         self.mail = mail
 
 
+    @classmethod
+    def log_in(cls, username=None, password=None):
+        from mentor import Mentor
+        from student import Student
 
-    def log_in(self, username, password):
         users = [Mentor.mentors_list,
                  Student.list_of_students]
 
-        if username in users:
+        for list_of_users in users:
+            for person in list_of_users:
+                if username == person.username:
+                    if password == person.password:
+                        return person
+        return False
+
+    @classmethod
+    def sign_out(cls):
+        # save data to file
+        cls.log_in()
 
 
+class Employee(User):
 
+    @classmethod
+    def add_employee(cls):
+        e = Employee(password, first_name, last_name, telephone, mail)
 
-        pass
-
-    def sign_out(self):
-        pass
 
