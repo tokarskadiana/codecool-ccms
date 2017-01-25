b9a8284785977550dcb89ad8e6ae0eddc7fb4e7f
pgurdek <pgurdek@gmail.com>
2017-01-25 14:29:38 +0100

    Added Assigment data read method

--- model/create_data.py
+++ model/create_data.py
@@ -2,6 +2,7 @@ from user import User
 # from mentor import Mentor
 from student import Student
 from assignment import Assignment
+from submit import Submition
 
 import csv
 
@@ -87,8 +88,9 @@ class Database(object):
 
             for element in assignment_reader:
                 count_record += 1
-                temp_data = Assignment(element[0], element[1], element[2], element[3], element[4])
-                object_assignment_list.append(temp_data)
+                cls.assigment_data_read(element)
+                # temp_data = Assignment(element[0], element[1], element[2], element[3], element[4])
+                # object_assignment_list.append(temp_data)
 
         return object_assignment_list
 
@@ -97,9 +99,24 @@ class Database(object):
 
         return [row.password,row.first_name,row.last_name,row.telephone,row.mail]
 
+    @staticmethod
+    def assigment_data_read(row,attr_number = 3):
+        # student_username
+        # content
+        # grade
+        # Structure title, description, due_date, submit_list
+        print(row)
+        new_list = row[3:]
+        submmit_object_list = []
+        for i in range(0,len(new_list),attr_number):
+            submmit_object_list.append(Submition(new_list[i],new_list[i+1],new_list[i+2]))
+        print(submmit_object_list)
+        return submmit_object_list
 
 
-# data = Database.create_user_from_csv('students.csv',Student)
+
+
+data = Database.create_assignment_from_csv('assigment.csv')
 #
 # data = Database.save_user_to_csv('students_test.csv',data)
 # # print(data[0].__dict__)
