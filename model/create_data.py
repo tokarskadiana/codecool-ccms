dfc8659fa9b7fbd2dcc4359fb661dbfa2503ab53
pgurdek <pgurdek@gmail.com>
2017-01-26 09:44:27 +0100

    Added Save to File Attendance

--- model/create_data.py
+++ model/create_data.py
@@ -72,6 +72,12 @@ class Database(object):
                 row = ','.join(cls.user_data(record))
                 file.write(row + "\n")
 
+    @staticmethod
+    def user_data(row):
+
+        return [row.password,row.first_name,row.last_name,row.telephone,row.mail]
+
+
     @classmethod
     def create_assignment_from_csv(cls,filename):
         """
@@ -114,10 +120,6 @@ class Database(object):
                 row = ','.join(cls.assignment_data_save(record))
                 file.write(row + "\n")
 
-    @staticmethod
-    def user_data(row):
-
-        return [row.password,row.first_name,row.last_name,row.telephone,row.mail]
 
     @staticmethod
     def assignment_data_read(row,attr_number = 3):
@@ -132,12 +134,79 @@ class Database(object):
 
     @staticmethod
     def assignment_data_save(row):
-        submilit_list = row.submit_list
+        attendance_list = row.submit_list
 
         submit = [row.title,row.description,row.due_date]
-        for index, item in enumerate(submilit_list):
+        for index, item in enumerate(attendance_list):
             submit.append(item.student_username)
             submit.append(item.content)
             submit.append(item.grade)
 
         return submit
+
+    @classmethod
+    def create_attendance_from_csv(cls, filename):
+        """
+        Static method for reading csv file and create list of users objects
+        :param filename:
+        :return: List of Objects
+        """
+        count_record = 0
+        file = cls.default_path + filename
+        object_attendance_list = []
+        with open(file, newline='') as csvfile:
+            attendance_reader = csv.reader(csvfile, delimiter=',')
+
+            for element in attendance_reader:
+                count_record += 1
+
+                temp_data = cls.attendance_data_read(element)
+                object_attendance_list.append(temp_data)
+                Attendance.add(temp_data)
+        return object_attendance_list
+
+
+    @staticmethod
+    def attendance_data_read(row,attr_number = 2):
+        new_list = row[1:]
+
+        attendance_object_dict = {}
+        for i in range(0,len(new_list),attr_number):
+            attendance_object_dict[new_list[i]] = new_list[i+1]
+
+        return Attendance(row[0],attendance_object_dict)
+
+    @classmethod
+    def save_attendance_to_csv(cls, filename , table):
+        """
+        Writes list of lists into a csv file.
+
+        Args:
+            file_name (str): name of file to write to
+            table: list of lists to write to a file
+
+        Returns:
+             None
+        """
+        file = cls.default_path + filename
+        print(table)
+        with open(file, "w") as file:
+            for record in table:
+
+                row = ','.join(cls.attendance_data_save(record))
+                file.write(row + "\n")
+
+
+    @staticmethod
+    def attendance_data_save(row):
+        print(row.__dict__)
+        attendance_list = row.student_presence
+
+        attendance = [row.date]
+        print(attendance_list)
+        for key , value in attendance_list.items():
+            attendance.append(key)
+            attendance.append(value)
+
+        return attendance
+
