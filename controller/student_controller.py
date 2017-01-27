5ceaf55dc8bbaeb6d7262615e0dc812d0c178617
pgurdek <pgurdek@gmail.com>
2017-01-27 10:03:47 +0100

    Added doc for student controller and view

--- controller/student_controller.py
+++ controller/student_controller.py
@@ -4,6 +4,7 @@ from view import View as view
 
 
 class StudentController(UserController):
+    """Class which controls user behaviour"""
 
     def list_assignment_grades(self):
         '''
@@ -20,6 +21,7 @@ class StudentController(UserController):
     def submit_assignment(self, assignment_title, content):
         '''
         Submit assignment as a student
+        return: string
         '''
         for assignment in Assignment.get_list():
             if assignment.get_title() == assignment_title:
@@ -32,6 +34,11 @@ class StudentController(UserController):
 
     @staticmethod
     def student_session(user):
+        """
+        Static method show menu of user
+        :param user:
+        :return: None
+        """
         session = StudentController(user)
         while True:
             view.student_menu()
--- view.py
+++ view.py
@@ -7,14 +7,23 @@ from controller.mentor_controller import MentorController
 
 
 class View:
-
+    """Class which have methods for UI"""
     @staticmethod
     def print_assignment_grades(list_assignment_grades):
+        """
+        Print assignments grades
+        :param list_assignment_grades: list
+        :return None
+        """
         for item in list_assignment_grades:
             print(item)
 
     @staticmethod
     def student_menu():
+        """
+        Student Menu
+        :return None
+        """
         View.clear()
         print('''
         -----------MENU-----------
@@ -25,13 +34,20 @@ class View:
 
     @staticmethod
     def print_assignment_grades(assignment_grades):
-        # headers = ['Assiment', 'Grade']
-        # print(tabulate(assignment_grades, headers,
-        #                tablefmt="fancy_grid", stralign="center"))
+        """
+        Show assignment grades
+        :param assignment_grades: list
+        :return None
+        """
         print(assignment_grades)
 
     @staticmethod
     def main_menu():
+        """
+        Show main menu
+        :param assignment_grades: list
+        :returns username,password
+        """
         View.clear()
         print('Welcome in CodeCool Management System')
         print('\nTo EXIT program press "0"\n')
@@ -45,6 +61,10 @@ class View:
 
     @staticmethod
     def mentor_menu():
+        """
+        Show mentor menu
+        :return: None
+        """
         print('''
         -----------MENU-----------
         1. Check attendance
@@ -59,11 +79,20 @@ class View:
 
     @staticmethod
     def display_assigments():
+        """
+        Display all assignments
+        :return: None
+        """
         for index, ass in enumerate((Assignment.list_assignment)):
             print('{} {} {}'.format(index, ass.title, ass.due_date))
 
     @staticmethod
     def display_ass(number):
+        """
+        Display assigment
+        :param number:
+        :return: bool
+        """
         details = MentorController.display_assignment(number)
         if details:
             for content in details:
@@ -72,18 +101,32 @@ class View:
 
     @staticmethod
     def display_students(students_list):
+        """
+        Show students list
+        :param students_list: list of students
+        :return: None
+        """
         for index, student in enumerate(students_list):
             print('{} {} {}'.format(index, student.first_name, student.last_name))
 
 
     @staticmethod
     def display_static_present(list_stat):
+        """
+        Show statistics of present of student
+        :param list_stat: list of stats
+        :return: None
+        """
         if list_stat:
             for key, value in list_stat.items():
                 print('{} {}'.format(key, value))
 
     @staticmethod
     def menager_menu():
+        """
+        Show manager menu
+        :return: None
+        """
         print('''
         -----------MENU-----------
         1. List mentors
@@ -96,6 +139,10 @@ class View:
 
     @staticmethod
     def employee_menu():
+        """
+        Show employee menu
+        :return: None
+        """
         View.clear()
         print('''
         -----------MENU-----------
@@ -105,11 +152,20 @@ class View:
 
     @staticmethod
     def print_mentors_list(mentors_list):
+        """
+        Show mentor list
+        :param mentors_list: list
+        :return:
+        """
         for mentor in mentors_list:
             print (mentor)
 
     @staticmethod
     def edit_menu():
+        """
+        Show edit menu for student
+        :return: None
+        """
         print('''
         You can edit following parameters:
             - telephone
@@ -118,6 +174,11 @@ class View:
 
     @staticmethod
     def show_full_name(user_list):
+        """
+        Show full name of user list
+        :param user_list: user list
+        :return: None
+        """
         View.clear()
         n = 0
         for user in user_list:
@@ -126,6 +187,11 @@ class View:
 
     @staticmethod
     def show_details(user):
+        """
+        Show details of user
+        :param user: list
+        :return: None
+        """
         print(
             '\n\t{} {}, phone number:{}, e-mail: {}'.format(user[0], user[1], user[3], user[4]))
 
@@ -134,5 +200,6 @@ class View:
     def clear():
         """
         Clears screen for better display
+        :return None
         """
         os.system('cls' if os.name == 'nt' else 'clear')
