f7290c66bd5a7b15b0eb12adc4d668921106f57d
Piotr <pgurdek@gmail.com>
2017-02-09 16:46:46 +0100

    Rly DUNNO

--- main.py
+++ main.py
@@ -13,14 +13,9 @@ from controller.employee_controller import EmployeeController
 
 def main():
     DatabaseController.createSqlDatabase()
-
+    DatabaseController.sample_data()
 
     log_in_input = View.main_menu()
-
-    DatabaseController.sample_data()
-    DatabaseController.sample_data()
-    DatabaseController.sample_data()
-
     username = log_in_input[0]
     password = log_in_input[1]
     user = UserController.log_in(username, password)
