009b95a2030e3903fa2bc77b26fbb55f793271f3
tokarskadiana <tokarskadiana@gmail.com>
2017-03-09 16:31:50 +0100

    Refactor code

--- model/manager.py
+++ model/manager.py
@@ -6,4 +6,20 @@ class Manager(Employee):
     """
     Represents manager object.
     """
-    pass
+
+    @classmethod
+    def get_by_id(cls, id):
+        """
+        Get manager by id from database.
+        arguments: int(manager id)
+        return: obj(Manager)
+        """
+        return super(Manager, cls).get_by_id(id, 'manager')
+
+    @classmethod
+    def list_managers(cls):
+        """
+        Returns list of managers object from database.
+        return: list(list of Manager objects)
+        """
+        return super(Manager, cls).list_employee('manager')
