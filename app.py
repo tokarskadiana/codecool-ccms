e09d0f568b6125d316f7cb83b775e5d370c8c6f4
tokarskadiana <tokarskadiana@gmail.com>
2017-03-09 16:50:37 +0100

    Delete __init__ files

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from model.attendance import Attendance
 from model.employee import Employee
 import datetime
 
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
@@ -462,9 +463,6 @@ def assignment_submit(assignments_id):
 
     return render_template('stud-submit.html', user=user_session(session['user'], session['type']),
                            assignment=assignment, submit=submit)
-
-
-# ======================= End assignments ==================================
 
 
 @app.route('/list-assignments/delete/<assignment_id>')
