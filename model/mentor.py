ddcbd069d628304629fa7ad4cc8d60aab613e0e1
tokarskadiana <tokarskadiana@gmail.com>
2017-03-09 14:35:26 +0100

    Merged

--- model/mentor.py
+++ model/mentor.py
@@ -6,19 +6,7 @@ class Mentor(Employee):
 
     @classmethod
     def get_by_id(cls, id):
-
-        query = 'SELECT * FROM employee WHERE id={} AND position = "{}"'.format(id, 'mentor')
-        employee = SqlRequest.sql_request(query)
-        if employee:
-            return cls(id=employee[0][0],
-                       password=employee[0][3],
-                       first_name=employee[0][1],
-                       last_name=employee[0][2],
-                       position=employee[0][8],
-                       telephone=employee[0][4],
-                       mail=employee[0][5],
-                       salary=employee[0][7])
-        return None
+        return super(Mentor, cls).get_by_id(id, 'mentor')
 
     @classmethod
     def list_mentors(cls):
