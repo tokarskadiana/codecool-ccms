198c9ab9c5cc730f760f2c0767385606c3a78caf
sebastiankurant <sebastian.kt7@gmail.com>
2017-03-22 21:50:53 +0100

    remove configuration methods to user

--- tests/assistant.py
+++ tests/assistant.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from tests.user import User
 
-
-class Assistant(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
-        self.base_url = "http://127.0.0.1:5000/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+class Assistant(User):
 
     def login_assistant(self):
         print('\tlogin test START')
@@ -61,37 +49,6 @@ class Assistant(unittest.TestCase):
         print('test list students')
         Assistant.login_assistant(self)
         Assistant.list_students(self)
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
 
 if __name__ == "__main__":
     unittest.main()
